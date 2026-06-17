6b885b07614e23aeda42476369e469834038ba74
Luiz Fernando S. <luiiz.fernandoo@icloud.com>
2026-06-17 02:23:00 -0300

    Feat: Portfólio em Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export", // Diz ao Next para gerar HTML/CSS/JS estáticos na pasta 'out'
+  basePath: "/luizfernando-portfolio", // IMPORTANTE: Coloque o nome exato do seu repositório no GitHub
+  images: {
+    unoptimized: true, // O GitHub Pages não suporta a otimização de imagens nativa do Next.js
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
